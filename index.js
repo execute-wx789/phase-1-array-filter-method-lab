fa63e41c05829244dee6594b710ec4edf5d1c621
Yurick <YJakse789@gmail.com>
2023-08-02 13:23:09 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, search){
+    return array.filter((found)=>{if(found.toLowerCase()===search.toLowerCase()){return found}})
+}
+function fuzzyMatch(array, search){
+    return array.filter((found)=>{
+        if(found.includes(search)&&search.includes(found.slice(0,1)))
+        {return found}
+    })
+}
+function matchName(array, search){
+    return array.filter((found)=>{if(found.name===search){return found}})
+}
